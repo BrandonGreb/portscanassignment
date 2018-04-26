3eb5724ead5597a30a84d661b7c9b3da9bc8f535
BrandonGreb <brandongreb@gmail.com>
2018-04-26 11:35:39 -0500

    Add files via upload

---
+++ Port Scanner 1.0_Greb.py
@@ -0,0 +1,46 @@
+#! python3
+# T L Byers
+#
+__date__ = "4/23/2018"
+'''*
+DESCRIPTION: Simple port scanner based on a Python 2.X design
+*'''
+
+import socket
+import sys
+from datetime import datetime
+
+
+remoteServer    = input("\nHost Name: ")
+remoteServerIP  = socket.gethostbyname(remoteServer)
+
+print("-" * 60)
+print("Please wait, scanning remote host", remoteServerIP)
+print("-" * 60)
+
+t1 = datetime.now()
+
+try:
+    for port in range(int(input("Enter Starting Port #: ")),
+                      int(input("Enter Ending Port #: "))):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((remoteServerIP, port))
+        if result == 0:
+            print("Port {0}: 	 Open".format(port))
+        sock.close()
+
+except socket.gaierror:
+    print('Hostname could not be resolved. Exiting')
+    sys.exit()
+
+except socket.error:
+    print("Couldn't connect to server")
+    sys.exit()
+
+t2 = datetime.now()
+
+total =  t2 - t1
+
+print("\nStart Time: {0}" .format(t1))
+print("\nEnd Time: {0}" .format(t2))
+print("\nScan Duration: {0}" .format(total))
